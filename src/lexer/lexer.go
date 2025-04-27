cb9657f46d0f20bebc49391674167c7cb499da3a
Alex Hendrix <hendrixtothemax38@gmail.com>
2025-04-26 21:18:46 -0600

    added two-character tokens

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -35,6 +35,14 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			liter := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: liter}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -60,7 +68,14 @@ func (l *Lexer) NextToken() token.Token {
 	case '/':
 		tok = newToken(token.DIVIDE, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			liter := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: liter}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -113,4 +128,12 @@ func (l *Lexer) readNumber() string {
 
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
